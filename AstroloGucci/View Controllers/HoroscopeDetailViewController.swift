5358e6621eb2a52f05929e9cdb8d574f9f496467
naomihimley <naomihimley@gmail.com>
2016-03-24 14:24:34 -0600

    removing unused folders, adding segmented control for yesterday, today, tmw

--- AstroloGucci/View Controllers/HoroscopeDetailViewController.swift
+++ AstroloGucci/View Controllers/HoroscopeDetailViewController.swift
@@ -13,6 +13,13 @@ class HoroscopeDetailViewController: UIViewController {
     @IBOutlet weak var signImageView: UIImageView!
     @IBOutlet weak var signAndDateLabel: UILabel!
     @IBOutlet weak var horoscopeTextView: UITextView!
+    @IBOutlet weak var dateSegmentedControl: UISegmentedControl!
+
+    enum ZodiacDates : Int {
+        case Yesterday = 0
+        case Today
+        case Tomorrow
+    }
 
     var zodiacSign : Zodiac?
     override func viewDidLoad() {
@@ -26,5 +33,20 @@ class HoroscopeDetailViewController: UIViewController {
         let todaysDate = NSDate()
         signAndDateLabel.text = "\(zodiacSign.name()) \n\(zodiacSign.dateSpanOfZodiac()) \n\(todaysDate.stringMMddyyyyFormat())"
         //TODO: Set horoscopeTextView's text from Gucci Mane lyrics generator once API is exposed
+    }
+
+    @IBAction func didSelectDateControl(sender: UISegmentedControl) {
+        let selectedDate = ZodiacDates(rawValue: dateSegmentedControl.selectedSegmentIndex)
+        switch selectedDate! {
+            case .Yesterday:
+                //TODO: Display Yesterday's horoscope for zodiacSign
+                DLog("Yesterday")
+            case .Today:
+                //TODO: Display Today's horoscope for zodiacSign
+                DLog("Today")
+            case .Tomorrow:
+                //TODO: Display Tomorrow's horoscope for zodiacSign
+                DLog("Tomorrow")
+        }
     }
 }
